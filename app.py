c7dbbf03fe97f56a5c73afa883a8717987dbcc00
Ruparam K <73575999+ruparam88@users.noreply.github.com>
2025-08-16 11:32:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -241,4 +241,4 @@ if __name__ == '__main__':
     if not os.path.exists('templates'):
         os.makedirs('templates')
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
